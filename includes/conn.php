7281d640be4f3e49c9050a0d4db75474126e072f
kaysiz <ksiziva@gmail.com>
2018-10-06 15:24:41 +0200

    created install.php and is working perfectly

--- includes/conn.php
+++ includes/conn.php
@@ -10,7 +10,6 @@ try {
     $conn = new PDO("mysql:host=$DB_DSN", $DB_USER, $DB_PASSWORD);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
 }
 catch(PDOException $e)
 {
